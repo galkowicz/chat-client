a5da8c8ef392c3dce9b5a8f4d17806da7fc9a362
Orian <orian@creditstacks.com>
2020-06-17 16:12:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import 'semantic-ui-css/semantic.min.css'
+
+import { Header } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as="h1">First Header</Header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
